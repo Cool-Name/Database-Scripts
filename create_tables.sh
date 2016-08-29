baf72ff496d44a78c8111b0bd0c7c78ae6ba9628
Nathan-Kelly <Partinarel@gmail.com>
2016-08-29 13:35:28 +0000

    script to create tables

---
+++ create_tables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#sqlite3 test2.db "PRAGMA foreign_keys = ON;"
+#->user        ([name] )
+#->blame       (*user,   date,    time,     message,  [id])
+#->environment ([title], blame*)
+#->role        (title,   [id],    env*,     blame*)
+#->repo        (hash,    label,   [id],     url,      dir,     blame*,  role*)
+
+sqlite3 $1 "PRAGMA foreign_keys = ON; CREATE TABLE user(name TEXT PRIMARY KEY NOT NULL);"
+sqlite3 $1 "PRAGMA foreign_keys = ON; CREATE TABLE blame (_user TEXT NOT NULL, date DATE NOT NULL, time TIME NOT NULL, message TEXT not null, id INT PRIMARY KEY NOT NULL, foreign key(_user) references user(name));"
+sqlite3 $1 "PRAGMA foreign_keys = ON; CREATE TABLE environment(title TEXT PRIMARY KEY NOT NULL, env_blame INT NOT NULL, foreign key(env_blame) references blame(id));"
+sqlite3 $1 "PRAGMA foreign_keys = ON; CREATE TABLE role(role_title TEXT NOT NULL, role_id INT PRIMARY KEY NOT NULL,  env TEXT NOT NULL, role_blame INT NOT NULL, foreign key(role_blame) references blame(id), foreign key(env) references environment(title));"
+sqlite3 $1 "PRAGMA foreign_keys = ON; CREATE TABLE repo(hash TEXT NOT NULL, label TEXT NOT NULL, repo_id INT PRIMARY KEY, url TEXT NOT NULL, dir TEXT NOT NULL, repo_blame INT NOT NULL, role_link INT not null, foreign key(repo_blame) references blame(id), foreign key(role_link) references role(role_id));"
