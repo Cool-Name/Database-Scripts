b83c1d29463363dfae9afae4f2b9a15a917d2c79
Nathan-Kelly <Partinarel@gmail.com>
2016-08-29 13:42:56 +0000

    add user to database

---
+++ add_user.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#->user        ([name] )
+#->blame       (*user,   date,    time,     message,  [id])
+#->environment ([title], blame*)
+#->role        (title,   [id],    env*,     blame*)
+#->repo        (hash,    label,   [id],     url,      dir,     blame*,  role*)
+
+sqlite3 $1 "PRAGMA foreign_keys = ON; insert into user values('$2');"
