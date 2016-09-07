c4ba109239d35bfc2171e521003a4fee4fa98cd8
Nathan-Kelly <Partinarel@gmail.com>
2016-09-07 03:12:09 +0000

    can add repo and roles - sanity checks soon

--- add_repo.sh
+++ add_repo.sh
@@ -9,3 +9,20 @@
 
 #todo: create blame, create repo, given a hash and label, auto_id, url, directory, and role pointer
 #      do it all in one transaction, so we can delete the blame if the title is in use
+DATE=$(date +"%s")
+DATABASE=$1
+USER=$2
+HASH=$3
+LABEL=$4
+URL=$5
+DIR=$6
+ROLE_ID=$7
+MESSAGE="Added repo $URL to role $ROLE_ID"
+echo "date:     "$DATE
+echo "DATABASE: "$DATABASE
+echo "USER:     "$USER
+echo "MESSAGE:  "$MESSAGE
+
+#arguments: database, user, hash, label, url, directory, role_id
+#arguments for repo: hash, label, [id], url, directory, blame*, role*)
+sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT COUNT(*) FROM blame)); INSERT INTO repo VALUES(\"$HASH\", \"$LABEL\", (SELECT COUNT(*) FROM repo), \"$URL\", \"DIR\", (SELECT COUNT(*) FROM blame) - 1, $ROLE_ID); commit;" && echo "SUCCESS"
--- add_role.sh
+++ add_role.sh
@@ -9,3 +9,15 @@
 
 #todo: create blame, then create role, add title, (id), point to env
 #      do it all in one transaction, so we can delete the blame if the title is in use
+DATE=$(date +"%s")
+DATABASE=$1
+TITLE=$2
+ENVIRONMENT=$3
+USER=$4
+MESSAGE="Added role $TITLE to environment $ENVIRONMENT"
+echo "date:     "$DATE
+echo "DATABASE: "$DATABASE
+echo "USER:     "$USER
+echo "MESSAGE:  "$MESSAGE
+#arguments: database, title, environement, user
+sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT COUNT(*) FROM blame)); INSERT INTO role VALUES(\"$TITLE\", (SELECT COUNT(*) FROM role), \"$ENVIRONMENT\", (SELECT COUNT(*) FROM blame) - 1); commit;" && echo "SUCCESS"
