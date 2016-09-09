79a73f608eab27d3a3b021ebc9d49cff07f70fc1
Nathan-Kelly <Partinarel@gmail.com>
2016-09-09 17:02:02 +1200

    can get list of environments from database

---
+++ list_environments.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#arguments needed: most likely just database - values returned are of PSV format
+
+ARGUMENTS="usage: ./list_environments [database]"
+DATABASE=$1
+ALL_ACTIONS_ALL_USERS="selecting all environments"
+
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied: database"
+    exit
+fi
+
+#check that a database was entered as an argument, and that that database really exists
+if [ -z "$DATABASE" ];
+then echo "no database supplied: database, [user]" && exit
+else if ! [ -f "$DATABASE" ] #check database exists
+then echo "database '$DATABASE' doesn't exist" && exit
+fi
+fi
+
+#select all actions from all users
+sqlite3 $DATABASE "-header" "-list" "select TITLE from environment;" && exit
+exit
