37abf3dbff3e6b65b979fa4e723859260c57426a
Nathan-Kelly <Partinarel@gmail.com>
2016-10-01 19:21:03 +1300

    fixed same thing as I did last time, before git broke it

--- add_repo.sh
+++ add_repo.sh
@@ -23,30 +23,29 @@ MESSAGE="Added repo $URL to role $ROLE_ID"
 #arguments for repo: hash, label, [id], url, directory, blame*, role*)
 if [ $# -eq 0 ]
 then
-    echo "No arguments supplied: database, user*, hash, label, url, directory, role_id*" && exit
-    exit
+    echo "No arguments supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 
 if [ -z "$1" ];
-then echo "no database supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no database supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$2" ];
-then echo "no user supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no user supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$3" ];
-then echo "no hash supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no hash supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$4" ];
-then echo "no label supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no label supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$5" ];
-then echo "no url supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no url supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$6" ];
-then echo "no directory supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no directory supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 if [ -z "$7" ];
-then echo "no role supplied: database, user*, hash, label, url, directory, role_id*" && exit
+then echo "no role supplied: database, user*, hash, label, url, directory, role_id*" && exit 1
 fi
 
 
@@ -54,15 +53,17 @@ if [ -f "$DATABASE" ] #check the database actually exists
 then
     if sqlite3 $DATABASE "select * from blame" >/dev/null #check the tables actually exist
     then #handle the missing vals
-	if (sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT max(id) FROM blame) + 1); INSERT INTO repo VALUES(\"$HASH\", \"$LABEL\", (SELECT max(repo_id) FROM repo) + 1, \"$URL\", \"$DIR\", (SELECT max(id) FROM blame), $ROLE_ID); commit;"  || sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT COUNT(*) FROM blame)); INSERT INTO repo VALUES(\"$HASH\", \"$LABEL\", 0, \"$URL\", \"$DIR\", (SELECT COUNT(*) FROM blame), $ROLE_ID); commit;") && echo "SUCCESS"
-	    then
-	        echo "date:     "$DATE
-		    echo "DATABASE: "$DATABASE
-		        echo "USER:     "$USER
-			    echo "ACTION:   "$MESSAGE
-			    fi
-    else echo "tables don't exist in database" && exit
+	if (sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT max(id) FROM blame) + 1); INSERT INTO repo VALUES(\"$HASH\", \"$LABEL\", (SELECT max(repo_id) FROM repo) + 1, \"$URL\", \"$DIR\", (SELECT max(id) FROM blame), $ROLE_ID); commit;"  || sqlite3 $DATABASE "PRAGMA foreign_keys = ON; begin; INSERT INTO blame VALUES('$USER', $DATE, '$MESSAGE', (SELECT max(id) FROM blame) + 1); INSERT INTO repo VALUES('$HASH', '$LABEL', 0, '$URL', '$DIR', (SELECT max(id) FROM blame), $ROLE_ID); commit;") && echo "SUCCESS"
+	then
+	    echo "date:     "$DATE
+	    echo "DATABASE: "$DATABASE
+	    echo "USER:     "$USER
+	    echo "ACTION:   "$MESSAGE
+	    exit 0
+	fi
+    else echo "tables don't exist in database" && exit 1
     fi
 else
-    echo "database doesn't exist" && exit
+    echo "database doesn't exist" && exit 1
 fi
+exit 1
